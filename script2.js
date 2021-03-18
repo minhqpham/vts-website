15540d97af7bf8ec37c1a63019d9090cde2607c5
minhqpham <72894427+minhqpham@users.noreply.github.com>
2021-03-18 10:15:53 -0400

    official info updated

--- script2.js
+++ script2.js
@@ -58,6 +58,116 @@ span4.onclick = function() {
   modal4.style.display = "none";
 }
 
+var modal5 = document.getElementsByClassName("modal")[5];
+var btn5 = document.getElementsByClassName("pichover")[5];
+var span5 = document.getElementsByClassName("close")[5];
+btn5.onclick = function() {
+  modal5.style.display = "block";
+}
+span5.onclick = function() {
+  modal5.style.display = "none";
+}
+
+var modal6 = document.getElementsByClassName("modal")[6];
+var btn6 = document.getElementsByClassName("pichover")[6];
+var span6 = document.getElementsByClassName("close")[6];
+btn6.onclick = function() {
+  modal6.style.display = "block";
+}
+span6.onclick = function() {
+  modal6.style.display = "none";
+}
+
+var modal7 = document.getElementsByClassName("modal")[7];
+var btn7 = document.getElementsByClassName("pichover")[7];
+var span7 = document.getElementsByClassName("close")[7];
+btn7.onclick = function() {
+  modal7.style.display = "block";
+}
+span7.onclick = function() {
+  modal7.style.display = "none";
+}
+
+var modal8 = document.getElementsByClassName("modal")[8];
+var btn8 = document.getElementsByClassName("pichover")[8];
+var span8 = document.getElementsByClassName("close")[8];
+btn8.onclick = function() {
+  modal8.style.display = "block";
+}
+span8.onclick = function() {
+  modal8.style.display = "none";
+}
+
+var modal9 = document.getElementsByClassName("modal")[9];
+var btn9 = document.getElementsByClassName("pichover")[9];
+var span9 = document.getElementsByClassName("close")[9];
+btn9.onclick = function() {
+  modal9.style.display = "block";
+}
+span9.onclick = function() {
+  modal9.style.display = "none";
+}
+
+var modal10 = document.getElementsByClassName("modal")[10];
+var btn10 = document.getElementsByClassName("pichover")[10];
+var span10 = document.getElementsByClassName("close")[10];
+btn10.onclick = function() {
+  modal10.style.display = "block";
+}
+span10.onclick = function() {
+  modal10.style.display = "none";
+}
+
+var modal11 = document.getElementsByClassName("modal")[11];
+var btn11 = document.getElementsByClassName("pichover")[11];
+var span11 = document.getElementsByClassName("close")[11];
+btn11.onclick = function() {
+  modal11.style.display = "block";
+}
+span11.onclick = function() {
+  modal11.style.display = "none";
+}
+
+var modal12 = document.getElementsByClassName("modal")[12];
+var btn12 = document.getElementsByClassName("pichover")[12];
+var span12 = document.getElementsByClassName("close")[12];
+btn12.onclick = function() {
+  modal12.style.display = "block";
+}
+span12.onclick = function() {
+  modal12.style.display = "none";
+}
+
+var modal13 = document.getElementsByClassName("modal")[13];
+var btn13 = document.getElementsByClassName("pichover")[13];
+var span13 = document.getElementsByClassName("close")[13];
+btn13.onclick = function() {
+  modal13.style.display = "block";
+}
+span13.onclick = function() {
+  modal13.style.display = "none";
+}
+
+var modal14 = document.getElementsByClassName("modal")[14];
+var btn14 = document.getElementsByClassName("pichover")[14];
+var span14 = document.getElementsByClassName("close")[14];
+btn14.onclick = function() {
+  modal14.style.display = "block";
+}
+span14.onclick = function() {
+  modal14.style.display = "none";
+}
+
+var modal15 = document.getElementsByClassName("modal")[15];
+var btn15 = document.getElementsByClassName("pichover")[15];
+var span15 = document.getElementsByClassName("close")[15];
+btn15.onclick = function() {
+  modal15.style.display = "block";
+}
+span15.onclick = function() {
+  modal15.style.display = "none";
+}
+
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
@@ -76,5 +186,38 @@ window.onclick = function(event) {
   }
   if (event.target == modal4) {
     modal4.style.display = "none";
+  }
+  if (event.target == modal5) {
+    modal5.style.display = "none";
+  }
+  if (event.target == modal6) {
+    modal6.style.display = "none";
+  }
+  if (event.target == modal7) {
+    modal7.style.display = "none";
+  }
+  if (event.target == modal8) {
+    modal8.style.display = "none";
+  }
+  if (event.target == modal9) {
+    modal9.style.display = "none";
+  }
+  if (event.target == modal10) {
+    modal10.style.display = "none";
+  }
+  if (event.target == modal11) {
+    modal11.style.display = "none";
+  }
+  if (event.target == modal12) {
+    modal12.style.display = "none";
+  }
+  if (event.target == modal13) {
+    modal13.style.display = "none";
+  }
+  if (event.target == modal14) {
+    modal14.style.display = "none";
+  }
+  if (event.target == modal15) {
+    modal15.style.display = "none";
   }
 }
